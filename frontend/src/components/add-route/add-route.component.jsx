de28382bc4caff3d173a90ebc5bcdcff7a0768d8
arikscherm <arikscherm@gmail.com>
2022-04-04 18:48:03 -0600

    Laid Out Add Route Page
    Laid Out Add Route Page. Submit button not working yet.

--- frontend/src/components/add-route/add-route.component.jsx
+++ frontend/src/components/add-route/add-route.component.jsx
@@ -2,89 +2,113 @@ import React, {useEffect, useState} from "react";
 import { TextField, Select, MenuItem } from "@mui/material";
 import axios from "axios";
 import "./add-route.styles.scss"
+import BasicSelect from "../basic-select/basic-select.component";
+import Button from '@mui/material/Button';
+import { Grid } from "@mui/material";
+
 
 const AddRoute = () => {
-    const[areaInfo, setAreaInfo] = useState([]);    //[stateVariable, functionThatUpdatesOurVariable]
-
-    useEffect(() => {   //This use effect grabs all the area data and puts it into the areaInfo state
-        axios.get('/api/areas/')
-        .then((res) => setAreaInfo(res.data))
-        .catch((err) => {
-            alert(err.message);
-        })
-    }, []);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(event.target.value)
+    const [color, setColor] = useState(''); 
+    const [area, setArea] = useState('');
+    const [areaInfo, setAreaInfo] = useState([]); 
+    const [setter, setSetter] = useState('');
+    const handleChangeColor = (event) => {
+      setColor(event.target.value);
     }
 
-    return(
+      const handleChangeArea = (event) => {
+        setArea(event.target.value);
+      };
+  
+           //[stateVariable, functionThatUpdatesOurVariable]
+
+
+        useEffect(() => {   //This use effect grabs all the area data and puts it into the areaInfo state
+            axios.get('/api/areas/')
+                .then((res) => setAreaInfo(res.data.map(area => area.name)))
+                .then(console.log(areaInfo))
+                .catch((err) => {
+                    alert(err.message);
+                })
+        }, []);
+
+        const handleSubmit = (event) => {
+                    event.preventDefault();
+                    alert(event.target.value)
+                 }
+    
+
+
+return (
+
     <form className="add-route-form" onSubmit={handleSubmit}>
-        <TextField 
-            name="grade"
-            className="form-input" 
-            label="Grade" 
-            type="text" 
-            placeholder="V0" 
-            required 
-            variant="standard"
-        >
-        </TextField>
-        <Select
-            name="color"
-            className="form-input"
-            label="Color"
-            value=""
-        >
-            <MenuItem value="">
-            <em>Pick A Color</em>
-            </MenuItem>
-            <MenuItem value={"Red"}>Red</MenuItem>
-            <MenuItem value={"Green"}>Green</MenuItem>
-            <MenuItem value={"Yellow"}>Yellow</MenuItem>
-            <MenuItem value={"Pink"}>Pink</MenuItem>
-            <MenuItem value={"Blue"}>Blue</MenuItem>
-        </Select>
-        <Select
-            name="area"
-            className="form-input"
-            label="Area"
-            value=""
-        >
-            <MenuItem value="">
-            <em>Choose An Area</em>
-            </MenuItem>
-            {
-                 areaInfo.map(({name}, index) => (
-                    <MenuItem key={index} value={name}>{name}</MenuItem>
-                ))
-            }
-        </Select>
-        <TextField 
-            name="description"
-            className="form-input"
-            label="Description" 
-            type="text" 
-            required 
-            variant="standard"
-        >
-        </TextField>
-        <Select
-            name="setter"
-            className="form-input"
-            label="Setter"
-            value=""
-        >
-            <MenuItem value="">
-            <em>Choose A Setter</em>
-            </MenuItem>
-            <MenuItem value={"Ari"}>Ari</MenuItem>
-            <MenuItem value={"Colin"}>Colin</MenuItem>
-        </Select>
-        <button>Submit me</button>
+        <Grid container spacing={5}>
+
+            <Grid item xs={6}>
+                <BasicSelect
+                    item={color}
+                    setItem={setColor}
+                    name='Color'
+                    listOfItems={
+                        ["Red", "Green", "Yellow", "Pink", "Blue"]
+                    }
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <BasicSelect
+                    item={area}
+                    setItem={setArea}
+                    name='Area'
+                    listOfItems={areaInfo}
+                />
+            </Grid>
+
+
+            <Grid item xs={6}>
+                <TextField
+                    name="Grade"
+                    className="form-input"
+                    label="Grade"
+                    type="text"
+                    placeholder="V0"
+                    required
+                    variant="standard"
+                    fullWidth
+                />
+
+            </Grid>
+
+            <Grid item xs={6}>
+                <TextField
+                    name="description"
+                    className="form-input"
+                    label="Description"
+                    type="text"
+                    required
+                    variant="standard"
+                    fullWidth
+                />
+
+            </Grid>
+
+            <Grid item xs={6}>
+                <BasicSelect
+                    item={setter}
+                    setItem={setSetter}
+                    name='Setter'
+                    listOfItems={
+                        ["Colin", "Ari"]
+                    }/>
+    
+            </Grid>
+
+        </Grid>
+        <div className="submit-button-container">
+        <Button variant="outlined" color="success" className="submit-button">Submit</Button>
+        </div>
     </form>
-    );
+  );
 }
+
 
 export default AddRoute
